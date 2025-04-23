a7ff3884d11d8bcc15221b21762697acb2120fc8
OnetrueMaple <theonetruemaple@proton.me>
2025-04-23 14:48:46 -0500

    Initial commit

---
+++ src/main/java/pain/TestingClient.java
@@ -0,0 +1,10 @@
+package pain;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
